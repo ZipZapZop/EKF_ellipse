307e74fca123ec89f90ba408b1668ab6f06f1d8f
ZipZapZop <niklas.hehenkamp@thi.de>
2018-08-20 16:17:27 +0200

    comments

--- generate_data.py
+++ generate_data.py
@@ -6,27 +6,29 @@ def polar_ellipse(theta, semimajor, semiminor):
     return (semimajor*semiminor)/(np.sqrt(denom))
 
 def generate_true_ellipse(num_trials, dt, semimajor, semiminor):
-    r = np.zeros(num_trials) # r
+    state = np.zeros((2,num_trials)) # [theta, r].T
     theta = np.linspace(0, 2*np.pi, num_trials)
     
-    
-    r[0] = polar_ellipse(theta[0], semimajor, semiminor)
+    state[0,0] = theta[0]
+    state[1,0] = polar_ellipse(theta[0], semimajor, semiminor)
     for i in range(1, num_trials):
-        r[i] = polar_ellipse(theta[i], semimajor, semiminor)
-    return r, theta
+        state[0,i] = theta[i]
+        state[1,i] = polar_ellipse(theta[i], semimajor, semiminor)
+    return state
 
 def generate_GPS_ellipse(num_trials, dt, semimajor, semiminor, std_dev_r):
-    r = np.zeros(num_trials)
+    state = np.zeros((2,num_trials)) # [theta, r].T
     theta = np.linspace(0, 2*np.pi, num_trials)
     for i in range(0, num_trials):
-        r[i] = polar_ellipse(theta[i], semimajor, semiminor) + np.random.normal(0, std_dev_r)
-    return r, theta
+        state[0,i] = theta[i]
+        state[1,i] = polar_ellipse(theta[i], semimajor, semiminor) + np.random.normal(0, std_dev_r)
+    return state
 
 def plotEllipse(num_trials, dt, semimajor, semiminor, std_dev_r):
-    r, theta = generate_GPS_ellipse(num_trials, dt, semimajor, semiminor, std_dev_r)
+    state = generate_GPS_ellipse(num_trials, dt, semimajor, semiminor, std_dev_r)
     
     plt.figure()
-    plt.polar(theta,r)
+    plt.polar(state[0],state[1])
     plt.show()
 
 plotEllipse(1000,0.001,200,100, 3)
