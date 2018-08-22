14c8b0855a0b43f446faa47ef4412bd528abb66a
ZipZapZop <niklas.hehenkamp@thi.de>
2018-08-22 12:39:54 +0200

    Added new Jacobian

--- conversion_tools.py
+++ conversion_tools.py
@@ -8,8 +8,13 @@ def cartesian_to_polar(polar_state):
     # cartesian_state[2] and cartesian_state[3] are 0 as sensor doesn't output v_x and v_y
     return cartesian_state
 
-def jacobian(r, theta):
-    return np.array([[np.cos(theta), -r*np.sin(theta)],
-                     [np.sin(theta), r*np.cos(theta)],
+def jacobian(polar_state):
+    return np.array([[np.cos(polar_state[0]), -polar_state[1]*np.sin(polar_state[0])],
+                     [np.sin(polar_state[0]), polar_state[1]*np.cos(polar_state[0])],
                      [0, 0],
                      [0, 0]])
+
+def new_jacobian(state): # comes in as 4x1 vector - [x, y, v_x, v_y].T
+    xsq_plus_ysq = state[0]**2 + state[1]**2
+    return np.array([[state[0]/np.sqrt(xsq_plus_ysq), state[1]/np.sqrt(xsq_plus_ysq), 0, 0],
+                     [-state[1]/xsq_plus_ysq, -state[0]/xsq_plus_ysq, 0, 0]])
